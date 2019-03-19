87c8328928a50b2bb5d914d39c03c4cecf711165
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 03:06:55 +0530

    Update TestFactory.java

--- web-api-appium-bdd-project/src/main/java/com/api/backend/executor/TestFactory.java
+++ web-api-appium-bdd-project/src/main/java/com/api/backend/executor/TestFactory.java
@@ -1,5 +1,8 @@
 package com.api.backend.executor;
 
 public class TestFactory {
-
+@Factory
+	public Object[] factoryMethod() {
+		return new Object[] {new ExcelDataProvider(),new DatabaseDataProvider()};	
+	}
 }
