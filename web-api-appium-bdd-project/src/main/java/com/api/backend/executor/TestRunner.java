0c93fe82a3f9cc08ec503d6b1cc35d8aea119e9c
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 03:06:03 +0530

    Update TestRunner.java

--- web-api-appium-bdd-project/src/main/java/com/api/backend/executor/TestRunner.java
+++ web-api-appium-bdd-project/src/main/java/com/api/backend/executor/TestRunner.java
@@ -1,5 +1,26 @@
 package com.api.backend.executor;
 
-public class TestRunner {
+@CucumberOptions(features = "Scenarios/api", 
+					glue = {"com.api.steps"}, 
+					format = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
+								"json:target/cucumber-reports/CucumberTestReport.json", 
+									"rerun:target/cucumber-reports/re-run.txt" })
 
+public class TestRunner {
+private TestNGCucumberRunner testRunner = new TestNGCucumberRunner(TestRunner.class);
+
+	@DataProvider(name = "features")
+	public Object[][] getFeatures() {
+		return testRunner.provideFeatures();
+	}
+
+	@Test(dataProvider = "features")
+	public void startExecution(CucumberFeatureWrapper cFeature) {
+		testRunner.runCucumber(cFeature.getCucumberFeature());
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		testRunner.finish();
+	}
 }
