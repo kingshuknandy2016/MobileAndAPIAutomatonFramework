5a4300d445c838227e5382193f68cd33fae8871e
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 03:02:32 +0530

    Update WebServiceTestBase.java

--- web-api-appium-bdd-project/src/main/java/com/api/backend/base/WebServiceTestBase.java
+++ web-api-appium-bdd-project/src/main/java/com/api/backend/base/WebServiceTestBase.java
@@ -1,5 +1,23 @@
 package com.api.backend.base;
 
 public class WebServiceTestBase {
+private static Response response;
 
+	public static Response getResponse() {
+		return response;
+	}
+
+	public static void setResponse(Response response) {
+		WebServiceTestBase.response = response;
+	}
+
+	public static Client getClient() {
+		Client client=Client.create();
+		client.addFilter(new CookiesFilter());
+		return client;
+	}
+	
+	public static WebResource getWebResource(String resource) {
+		return  getClient().resource(ConfigurationManager.getBundle().getProperty("env.url")).path(resource);
+	}
 }
