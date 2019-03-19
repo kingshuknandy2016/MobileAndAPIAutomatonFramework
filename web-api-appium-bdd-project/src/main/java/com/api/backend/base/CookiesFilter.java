ed9a6d4dde542e51db83964e30816e0585f28f45
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 02:53:52 +0530

    Update CookiesFilter.java

--- web-api-appium-bdd-project/src/main/java/com/api/backend/base/CookiesFilter.java
+++ web-api-appium-bdd-project/src/main/java/com/api/backend/base/CookiesFilter.java
@@ -1,5 +1,18 @@
 package com.api.backend.base;
 
-public class CookiesFilter {
+public class CookiesFilter extends ClientFilter{
+
+	@Override
+	public ClientResponse handle(ClientRequest request) throws ClientHandlerException {
+		System.out.println("Request URL:  " + request.getURI());
+		ClientResponse clientResponse = getNext().handle(request);
+		Response response=new Response(clientResponse);
+		System.out.println("Response Code:  " + response.getStatus().getStatusCode());
+		System.out.println("Response Phrase:  " + response.getStatus().getReasonPhrase());
+		System.out.println("Response Body:  " + response.getMessageBody());
+		WebServiceTestBase.setResponse(response);
+		return clientResponse;
+	}
 
 }
+
