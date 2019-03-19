8036b66810beebfe64a9cd47e882f5ea257f57d0
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 03:07:58 +0530

    Update DatabaseDataProvider.java

--- web-api-appium-bdd-project/src/main/java/com/api/dataprovider/DatabaseDataProvider.java
+++ web-api-appium-bdd-project/src/main/java/com/api/dataprovider/DatabaseDataProvider.java
@@ -1,5 +1,9 @@
 package com.api.dataprovider;
 
 public class DatabaseDataProvider {
-
+	@DataProvider(name="dataProviderDB")
+	public Object[][] getDataFromDB() {
+		Object object[][]=DatabaseUtils.getData("select * from orders");
+		return object;
+	}
 }
