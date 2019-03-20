d1ebea791c5a060a16e440f8b6c414cd1a1ae733
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 23:10:29 +0530

    Update OrderServices.java

--- web-api-appium-bdd-project/src/main/java/com/api/steps/OrderServices.java
+++ web-api-appium-bdd-project/src/main/java/com/api/steps/OrderServices.java
@@ -1,5 +1,23 @@
 package com.api.steps;
 
 public class OrderServices {
-
+public static void getOrders() {
+		getWebResource("/orders.json").get(ClientResponse.class);
+	}
+	
+	public static void addOrders(String clientName,String amount) {
+		String entity=String.format("{'clientName':'%s','amount':'%s'}", clientName,amount);
+		getWebResource("/orders.json").entity(entity).post(ClientResponse.class);
+	}
+	
+	public static void deleteOrder(String orderId) {
+		String entity=String.format("/orders/%s.json", orderId);
+		getWebResource(entity).delete(ClientResponse.class);
+	}
+	
+	public static void editOrder(String orderId,String clientName,String amount) {
+		String entity=String.format("{'clientName':'%s','amount':'%s'}", clientName,amount);
+		String resource=String.format("/orders/%s.json", orderId);
+		getWebResource(resource).entity(entity).put(ClientResponse.class);
+	}
 }
