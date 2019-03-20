6acd44514a647b665daab007750a023662d4c044
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 23:11:02 +0530

    Update Steps.java

--- web-api-appium-bdd-project/src/main/java/com/api/steps/Steps.java
+++ web-api-appium-bdd-project/src/main/java/com/api/steps/Steps.java
@@ -1,5 +1,13 @@
 package com.api.steps;
 
 public class Steps {
-
+@Given("^Get order details$")
+	public static void get_Orders() {
+		OrderServices.getOrders();
+	}
+	
+	@Then("^verify response code \"([^\"]*)\"$")
+	public void verifyResponseCode(String code) {		
+		System.out.println("Response:"+OrderServices.getResponse().getStatus().getStatusCode());
+	}
 }
