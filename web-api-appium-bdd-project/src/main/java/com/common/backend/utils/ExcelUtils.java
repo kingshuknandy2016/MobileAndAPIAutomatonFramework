604b3ad4d75370796a619df06aec3b713d410b5d
kingshuknandy2016 <36564770+kingshuknandy2016@users.noreply.github.com>
2019-03-20 22:40:31 +0530

    Update ExcelUtils.java

--- web-api-appium-bdd-project/src/main/java/com/common/backend/utils/ExcelUtils.java
+++ web-api-appium-bdd-project/src/main/java/com/common/backend/utils/ExcelUtils.java
@@ -1,5 +1,52 @@
 package com.common.backend.utils;
 
 public class ExcelUtils {
-
+public String filePath;
+	public String sheetName;
+	
+    public ExcelUtils(String filePath,String sheetName) {
+		this.filePath=filePath;
+		this.sheetName=sheetName;
+	}
+    
+  public Object getCellData(int rowNum,int cellNum) {
+    	Object outPut = null;
+    	try {
+			FileInputStream fis=new FileInputStream(filePath);
+			HSSFWorkbook workbook=new HSSFWorkbook(fis);
+			HSSFSheet sheet=workbook.getSheet(sheetName);
+			HSSFRow row=sheet.getRow(rowNum);
+			HSSFCell cell=row.getCell(cellNum);
+			
+			if (cell.getCellType().toString().contains("STRING")) {
+				outPut = cell.getStringCellValue();
+			} else if (cell.getCellType().toString().contains("NUMERIC")) {
+				outPut = cell.getNumericCellValue();
+			}
+			System.out.println(outPut);
+    	} catch (Exception e) {
+			System.out.println("Exception Occured. Error:"+e.getMessage());
+		}
+		return outPut;
+    }
+  
+   public int getRowCount() {
+    	int number = 0;
+    	try {
+			FileInputStream fis=new FileInputStream(filePath);
+			HSSFWorkbook workbook=new HSSFWorkbook(fis);
+			HSSFSheet sheet=workbook.getSheet(sheetName);
+			number=sheet.getPhysicalNumberOfRows();
+			System.out.println("RowCount:"+number);
+    	} catch (Exception e) {
+			System.out.println("Exception Occured. Error:"+e.getMessage());
+		}
+		return number;
+    }
+  
+    
+    public static void main(String[] args) {
+    	ExcelUtils data=new ExcelUtils(System.getProperty("user.dir")+"\\resources\\"+ConfigurationManager.getBundle().getProperty("data.provider").toString(), "userDetails");
+    	data.getCellData(1,0);
+	}
 }
